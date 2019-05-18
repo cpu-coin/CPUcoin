59be63422c9f97ddd701fe33a12ff0bd81694d60
David Allen Jennings <davidjennings@gmail.com>
2019-05-17 17:33:45 -0700

    Remove duplicate copy of folder, caused by initial mis-add

--- CPUcoin/keccak256.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-const sha3 = require('js-sha3');
-
-const password = fs.readFileSync(path.resolve('./', 'hash-me.txt'), 'utf8');
-
-const encrypted = sha3.keccak256(password);
-console.log('Keccak256(password):', encrypted, 'length:', encrypted.length);
